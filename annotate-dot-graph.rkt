2a2ee98abc65f5fc37449715470d2f7867e013f6
Daniel King <danking@ccs.neu.edu>
2013-05-09 19:29:24 -0400

    New procedures to add summary and push-pop-web edges to digraphs

---
+++ annotate-dot-graph.rkt
@@ -0,0 +1,55 @@
+#lang racket
+
+(require "make-dot-graph.rkt"
+         "dot-graph-data.rkt")
+(provide add-summary-edges)
+
+(define default-summary-edge-attributes
+  (hash 'style "dashed" 'color "red"))
+(define default-web-edge-attributes
+  (hash 'style "dotted" 'color "blue"))
+
+;; add-summary-edges : [SetOf [List UID UID]] Digraph -> Digraph
+;;
+;; adds the edges from a UID summary set
+(define (add-summary-edges summaries g)
+  (for/fold
+      ((g g))
+      ((sum summaries))
+    (add-uid-edge g (first sum) (second sum))))
+
+;; add-uid-edge : UID UID Digraph -> Digraph
+;;
+;; Produces a new digraph with an edge pointing from the first uid to the second
+;; one.
+(define (add-uid-edge from to g)
+  (add-edge g (uid->node-name from) (uid->node-name to)
+            default-summary-edge-attributes))
+
+;; add-webset-edges : [SetOf [Web UID UID]] Digraph -> Digraph
+;;
+;; Adds the edges implied by each web from the webset to the digraph. These
+;; edges enable the viewer to easily see interrelationships between push and pop
+;; nodes.
+(define (add-webset-edges webset g)
+  (for/fold
+      ((g g))
+      ((web webset))
+    (add-web-edges g web)))
+
+;; add-web-edges : [Web UID UID] Digraph -> Digraph
+;;
+;; Adds an edge from every push to every pop
+(define (add-web-edges web g)
+  (for*/fold
+      ((g g))
+      ((from (web-pushes g))
+       (to (web-pops g)))
+    (add-web-edge from to g)))
+
+;; add-web-edge : UID UID Digraph -> Digraph
+;;
+;; Adds an edge from the first UID to the second
+(define (add-web-edge form to g)
+  (add-edge g (uid->node-name from) (uid->node-name to)
+            default-web-edge-attributes))

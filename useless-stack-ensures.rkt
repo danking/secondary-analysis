54d40fd6cad70396d0c7f10d1f503081c619651f
Daniel King <danking@ccs.neu.edu>
2013-06-10 15:02:38 -0400

    Fix useless-stack-ensures

--- useless-stack-ensures.rkt
+++ useless-stack-ensures.rkt
@@ -1,16 +1,21 @@
 #lang racket
 
 (require "../pda-to-pda-risc/risc-enhanced/search.rkt"
-         "../pda-to-pda-risc/risc-enhanced/data.rkt")
+         "../pda-to-pda-risc/risc-enhanced/data.rkt"
+         "../cfa2-analyses/min-headroom.rkt"
+         "../lattice/lattice.rkt"
+         )
 (provide useless-stack-ensures)
+
+(define headroom-gte? (lattice-gte? min-headroom-bounded-lattice))
 
 (define (useless-stack-ensures pre uid->min-hdrm)
   (define (combine t useless-ensures)
     (let ((i (pda-term-insn t)))
-      (if (or (not (stack-ensure? i))
-              (> (stack-ensure-hdrm i)
-                 (uid->min-hdrm (get-uid i))))
-          useless-ensures
-          (cons t useless-ensures))))
+      (if (and (stack-ensure? i)
+               (headroom-gte? (uid->min-hdrm (get-uid i))
+                              (stack-ensure-hdrm i)))
+          (cons t useless-ensures)
+          useless-ensures)))
 
   (folding-forward-search combine empty (pda-risc-enh-initial-term pre)))
